1f5838e5827be2e9fe293c2415190dd032b6a1bf
Christian Payne <67926222+ChrisPayneHome@users.noreply.github.com>
2021-02-23 01:49:58 +0000

    Add files via upload

---
+++ scammer.py
@@ -0,0 +1,40 @@
+import requests
+import os
+import random
+import string 
+import json
+import time
+
+
+random.seed = (os.urandom(1026))
+
+url = 'https://paypal-login960.com/returnUri/signin'
+
+email_end = ('@yahoo.com', '@outlook.com', '@gmail.com', '@harvard.edu', '@email.com', '@oxford.ac.uk', '@yale.edu', '@outlook.co.uk', '@mit.edu', '@Hotmail.com', '@zoho.com', '@mail.com')
+
+names = json.loads(open('names.json').read())
+words = json.loads(open('words.json').read())
+
+for name in names:
+	
+	name_extra = ''.join(random.choice(string.digits))
+
+	email = name.lower() + name_extra + random.choice(email_end)
+	password = random.choice(words) + str(random.randint(1950, 2021))
+	
+	try:
+		requests.post(url, allow_redirects = False, data = {
+			'EML': email,
+			'PWD': password
+		})
+
+		print('Sending email: %s and password: %s' % (email, password))
+
+	except Exception:
+		print('Encountered error, beginning again')
+		time.sleep(2)
+
+	
+
+
+
